08f516bad2a605884c880715fdd9ba26bf9bf435
Ivan Ferraro <ivanferrao@MacBook-Pro-di-Ivan.local>
2020-06-06 16:23:57 +0200

    Remove some code

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-import math
-import sys
-from os import rename
+import requests
 
 
-name = input("Your name? ")
-print("Hello, ", name)
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+
+r = requests.get("https://coreyms.com")
+print(r.status_code)
